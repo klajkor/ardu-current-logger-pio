69eeb29c2fde827aaaea0e4b2ad8194a4ea571f7
Robert Klajko <klajkor@gmail.com>
2019-10-24 16:37:17 +0200

    Readme update

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Arduino current logger with INA219 I2C module
 
-Modules used:
+Board: Arduino Nano
+
+Extension modules used:
 - INA219, I2C
 - DS3231 RTC, I2C
 - SSD1306 OLED dsiplay, I2C
 - SD card, SPI
 
 Libraries used:
-- Adafruit INA219 by Adafruit
-- SD by Adafruit Industries
-- SSD1306Ascii by Bill Greiman
-- uRTCLib by Naguissa
+- Adafruit INA219 by Adafruit - Copyright (c) 2012, Adafruit Industries
+- SD by Adafruit Industries - Copyright (c) 2012, Adafruit Industries
+- SSD1306Ascii by Bill Greiman - Copyright (c) 2019, Bill Greiman
+- uRTCLib by Naguissa - Copyright (c) 2019, Naguissa
 
 Arduino Nano pinout connections
 
@@ -26,3 +28,4 @@ SD card on SPI bus:
 
 Toolchain: VSCode + Platform.IO
 
+Copyright (c) 2019, Robert Klajko
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,43 @@
-/**********************************************
-** Arduino current meter and logger
-**
-** Modules used:
-**  - INA219, I2C
-**  - DS3231 RTC, I2C
-**  - SSD1306 OLED dsiplay, I2C
-**  - SD card, SPI
-** Libraries used:
-**  - Adafruit INA219 by Adafruit
-**  - SD by Adafruit Industries
-**  - SSD1306Ascii by Bill Greiman
-**  - uRTCLib by Naguissa
-**********************************************/
-
-/* Arduino Nano pinout connections
-
- ** I2C bus:
- ** SCK - pin A5
- ** SDA - pin A4
-
- ** SD card on SPI bus:
- ** MOSI - pin D11
- ** MISO - pin D12
- ** CLK - pin D13
- ** CS - pin D4
+/**
+* Arduino current meter and logger
+*
+* @author Robert Klajko
+* @url https://github.com/klajkor/ardu-current-logger-pio
+*
+* Board: Arduino Nano
+*
+* Extension modules used:
+*  - INA219, I2C
+*  - DS3231 RTC, I2C
+*  - SSD1306 OLED dsiplay, I2C
+*  - SD card, SPI
+* Libraries used:
+*  - Adafruit INA219 by Adafruit - Copyright (c) 2012, Adafruit Industries
+*  - SD by Adafruit Industries - Copyright (c) 2012, Adafruit Industries
+*  - SSD1306Ascii by Bill Greiman - Copyright (c) 2019, Bill Greiman
+*  - uRTCLib by Naguissa - Copyright (c) 2019, Naguissa
+*
+* BSD license, all text here must be included in any redistribution.
+*
 */
 
-/* Toolchain: VSCode + Platform.IO */
+/** 
+* Arduino Nano pinout connections
+*
+* I2C bus:
+* - SCK - pin A5
+* - SDA - pin A4
+*
+* SD card on SPI bus:
+* - MOSI - pin D11
+* - MISO - pin D12
+* - CLK - pin D13
+* - CS - pin D4
+*/
+
+/**
+ * Toolchain: VSCode + Platform.IO 
+ */
 
 #include <Arduino.h>
 #include <avr/pgmspace.h>
